0f5a3ea1914e507e1b95d7a414bdd421b801eb8d
lostKoder <lostkoder@gmail.com>
2018-01-14 21:03:08 +0330

    changes

--- bin/flush_proxies.sh
+++ bin/flush_proxies.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-files=$(mysql --database monitor -e' select config_file from tor_proxies where enabled = 0;'  | tail -n +2)
+files=$(/usr/bin/mysql --database monitor -e' select config_file from tor_proxies where enabled = 0;'  | /usr/bin/tail -n +2)
 
 for file in ${files}
 do
-    kill -9 $(ps aux | grep ${file} | grep -v grep | sed -r 's/ +/\t/g' | cut -f2)
+    kill -9 $(/bin/ps aux | /bin/grep ${file} | /bin/grep -v grep | /bin/sed -r 's/ +/\t/g' | /usr/bin/cut -f2)
     tor -f ${file} &
-    mysql --database monitor -e"update tor_proxies set enabled = 1 where config_file='${file}'"
+    /usr/bin/mysql --database monitor -e"update tor_proxies set enabled = 1 where config_file='${file}'"
 done
