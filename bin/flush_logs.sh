e26fdb6ab82dceb0fc5f283d51162b09c709a42e
lostkoder <lostkoder@gmail.com>
2018-01-15 15:36:23 +0330

    changes

--- bin/flush_logs.sh
+++ bin/flush_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/mysql --database monitor -e'delete from logs where UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 8 day))'
+/usr/bin/mysql --database monitor -e'delete from logs where time < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 8 DAY))'
