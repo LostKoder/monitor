9c029f80f8d2b9f038ce9527b45deb119480b4d4
lostkoder <lostkoder@gmail.com>
2018-01-22 13:34:15 +0330

    google changes

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,6 +10,7 @@ namespace Core;
 
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -35,28 +36,24 @@ class Kernel
      */
     public function handle(Request $request)
     {
-        $origin = $request->server->get('HTTP_ORIGIN');
-        // return bullshit if it was not from rc or main server
-        if (!in_array($origin,['https://malltina.com', 'https://rc1.malltina.com']) && !$request->query->has('imboss')) {
-            return new Response(sha1(time()));
-        }
-
         // prepare parameters
         $method = $request->getMethod();
 
         // make request to the amazon
-        $url = 'https://www.amazon.com'.$request->getRequestUri();
+        $url = 'https://www.google.com'.$request->getRequestUri();
 
-        $response = $this->client->request($method, $url);
-
+        try {
+            $response = $this->client->request($method, $url);
+        } catch (RequestException $e) {
+            $response = $e->getResponse();
+        }
         $headers = [
-          'access-control-allow-origin' => $origin,
           'cf-ray' => '3db09a8eed929be7-AMS',
           'vary' => 'Accept-Encoding',
         ];
 
         $contents = $response->getBody()->getContents();
 
-        return new JsonResponse(['response' => $contents], 200, $headers);
+        return new Response($contents, $response->getStatusCode(), $headers);
     }
 }
--- src/Proxy/ClientProxy.php
+++ src/Proxy/ClientProxy.php
@@ -9,64 +9,19 @@
 namespace Core\Proxy;
 
 
-use Carbon\Carbon;
 use Core\Factory\LoggerFactory;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\RequestException;
 
 class ClientProxy extends Client
 {
     public function request($method, $uri = '', array $options = [])
     {
-        $options['connect_timeout'] = 5;
-        foreach ($this->proxies() as $proxy) {
-            $options = array_merge($options,['proxy' => $proxy->address]);
-            try {
-                $proxy->used_at = Carbon::now();
-                $proxy->save();
-                $start = microtime(true);
-                $response = parent::request($method, $uri, $options);
-                $this->logger()->debug('Proxy connection succeed',['duration' => microtime(true) - $start]);
-                return $response;
-            } catch (RequestException $e) {
-                // if error was not 404 or 503 mark proxy as failed
-                if (in_array($e->getCode(), [404, 503, 200])) {
-                    throw $e;
-                }
-
-                $this->logger()->debug('Proxy connection failed',[
-                    'exception' => $e->getMessage(),
-                    'code' => $e->getCode()
-                ]);
-
-
-                $this->proxyFailureHandler()->handle($proxy);
-                continue;
-            }
+            $options = array_merge($options,['proxy' => 'socks5://127.0.0.1:9050']);
+            $start = microtime(true);
+            $response = parent::request($method, $uri, $options);
+            $this->logger()->debug('Proxy connection succeed',['duration' => microtime(true) - $start]);
+            return $response;
         }
-    }
-
-    /**
-     * @return TorProxy[]
-     */
-    private function proxies(){
-
-        /** @var TorProxy[] $proxies */
-        $proxies = TorProxy::query()
-            ->orderBy('used_at','asc')
-            ->where('enabled', true)
-            ->get();
-
-        return $proxies;
-    }
-
-    /**
-     * @return ProxyFailureHandler
-     */
-    private function proxyFailureHandler()
-    {
-        return new ProxyFailureHandler();
-    }
 
     /**
      * @return \Psr\Log\LoggerInterface
